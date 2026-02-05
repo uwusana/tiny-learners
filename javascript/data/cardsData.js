bd2c7bbc2595a268013a085b58e882b0763ebfa5
Upasana Mukherjee <upasanadesigns03@gmail.com>
2026-02-05 23:35:33 +0530

    Refactor code structure for improved readability and maintainability

--- javascript/data/cardsData.js
+++ javascript/data/cardsData.js
@@ -29,84 +29,85 @@ const cardsData = Object.freeze( {
   ],
 
   numbers: [
-    { id: 1, title: "1", image: "assets/images/cards/numbers/1.png" },
-    { id: 2, title: "2", image: "assets/images/cards/numbers/2.png" },
-    { id: 3, title: "3", image: "assets/images/cards/numbers/3.png" },
-    { id: 4, title: "4", image: "assets/images/cards/numbers/4.png" },
-    { id: 5, title: "5", image: "assets/images/cards/numbers/5.png" }, 
-    { id: 6, title: "6", image: "assets/images/cards/numbers/6.png" },
-    { id: 7, title: "7", image: "assets/images/cards/numbers/7.png" },
-    { id: 8, title: "8", image: "assets/images/cards/numbers/8.png" },
-    { id: 9, title: "9", image: "assets/images/cards/numbers/9.png" },
-    { id: 10, title: "10", image: "assets/images/cards/numbers/10.png" }
+    { id: 1, title: "1", image: "assets/images/numbers/1.png" },
+    { id: 2, title: "2", image: "assets/images/numbers/2.png" },
+    { id: 3, title: "3", image: "assets/images/numbers/3.png" },
+    { id: 4, title: "4", image: "assets/images/numbers/4.png" },
+    { id: 5, title: "5", image: "assets/images/numbers/5.png" }, 
+    { id: 6, title: "6", image: "assets/images/numbers/6.png" },
+    { id: 7, title: "7", image: "assets/images/numbers/7.png" },
+    { id: 8, title: "8", image: "assets/images/numbers/8.png" },
+    { id: 9, title: "9", image: "assets/images/numbers/9.png" },
+    { id: 10, title: "10", image: "assets/images/numbers/10.png" }
   ],
 
   fruits: [
-    { id: 1, title: "Apple", image: "assets/images/cards/fruits/apple.png" },
-    { id: 2, title: "Banana", image: "assets/images/cards/fruits/banana.png" },
-    { id: 3, title: "Orange", image: "assets/images/cards/fruits/orange.png" },
-    { id: 4, title: "Grapes", image: "assets/images/cards/fruits/grapes.png" },
-    { id: 5, title: "Watermelon", image: "assets/images/cards/fruits/watermelon.png" }
+    { id: 1, title: "Apple", image: "assets/images/fruits/apple.png" },
+    { id: 2, title: "Banana", image: "assets/images/fruits/banana.png" },
+    { id: 3, title: "Orange", image: "assets/images/fruits/orange.png" },
+    { id: 4, title: "Grapes", image: "assets/images/fruits/grapes.png" },
+    { id: 5, title: "Watermelon", image: "assets/images/fruits/watermelon.png" }
 
   ],
 
   colors: [
-    { id: 1, title: "Red", image: "assets/images/cards/colors/red.png" },
-    { id: 2, title: "Blue", image: "assets/images/cards/colors/blue.png" },
-    { id: 3, title: "Green", image: "assets/images/cards/colors/green.png" },
-    { id: 4, title: "Yellow", image: "assets/images/cards/colors/yellow.png" },
-    { id: 5, title: "Purple", image: "assets/images/cards/colors/purple.png" },
-    { id: 6, title: "Orange", image: "assets/images/cards/colors/orange.png" },
-    { id: 7, title: "Pink", image: "assets/images/cards/colors/pink.png" },
-    {id: 8, title: "White", image: "assets/images/cards/colors/white.png" },
-    { id: 9, title: "Black", image: "assets/images/cards/colors/black.png" }
+    { id: 1, title: "Red", image: "assets/images/colors/red.png" },
+    { id: 2, title: "Blue", image: "assets/images/colors/blue.png" },
+    { id: 3, title: "Green", image: "assets/images/colors/green.png" },
+    { id: 4, title: "Yellow", image: "assets/images/colors/yellow.png" },
+    { id: 5, title: "Purple", image: "assets/images/colors/purple.png" },
+    { id: 6, title: "Orange", image: "assets/images/colors/orange.png" },
+    { id: 7, title: "Pink", image: "assets/images/colors/pink.png" },
+    {id: 8, title: "White", image: "assets/images/colors/white.png" },
+    { id: 9, title: "Black", image: "assets/images/colors/black.png" }
   ],
 
   shapes: [
-    { id: 1, title: "Circle", image: "assets/images/cards/shapes/circle.png" },
-    { id: 2, title: "Square", image: "assets/images/cards/shapes/square.png" },
-    { id: 3, title: "Triangle", image: "assets/images/cards/shapes/triangle.png" },
-    { id: 4, title: "Rectangle", image: "assets/images/cards/shapes/rectangle.png" },
-    { id: 5, title: "Star", image: "assets/images/cards/shapes/star.png" },
-    { id: 6, title: "Heart", image: "assets/images/cards/shapes/heart.png" }
+    { id: 1, title: "Circle", image: "assets/images/shapes/circle.png" },
+    { id: 2, title: "Square", image: "assets/images/shapes/square.png" },
+    { id: 3, title: "Triangle", image: "assets/images/shapes/triangle.png" },
+    { id: 4, title: "Rectangle", image: "assets/images/shapes/rectangle.png" },
+    { id: 5, title: "Star", image: "assets/images/shapes/star.png" },
+    { id: 6, title: "Heart", image: "assets/images/shapes/heart.png" }
 
   ],
 
   animals: [
-    { id: 1, title: "Dog", image: "assets/images/cards/animals/dog.png" },
-    { id: 2, title: "Cat", image: "assets/images/cards/animals/cat.png" },
-    { id: 3, title: "Elephant", image: "assets/images/cards/animals/elephant.png" },
-    { id: 4, title: "Lion", image: "assets/images/cards/animals/lion.png" },
-    { id: 5, title: "Tiger", image: "assets/images/cards/animals/tiger.png" },
-    { id: 6, title: "Monkey", image: "assets/images/cards/animals/monkey.png" },
-    { id: 7, title: "Rabbit", image: "assets/images/cards/animals/rabbit.png" }
+    { id: 1, title: "Dog", image: "assets/images/animals/dog.png" },
+    { id: 2, title: "Cat", image: "assets/images/animals/cat.png" },
+    { id: 3, title: "Fish", image: "assets/images/animals/fish.png" },
+    { id: 4, title: "Lion", image: "assets/images/animals/lion.png" },
+    { id: 5, title: "Snake", image: "assets/images/animals/snake.png" },
+    { id: 6, title: "Monkey", image: "assets/images/animals/monkey.png" },
+    { id: 7, title: "Rabbit", image: "assets/images/animals/rabbit.png" },
+    { id: 8, title: "Cow", image: "assets/images/animals/cow.png" }
   ],
 
   emotions: [
-    { id: 1, title: "Happy", image: "assets/images/cards/emotions/happy.png" },
-    { id: 2, title: "Sad", image: "assets/images/cards/emotions/sad.png" },
-    { id: 3, title: "Angry", image: "assets/images/cards/emotions/angry.png" },
-    { id: 4, title: "Surprised", image: "assets/images/cards/emotions/surprised.png" },
-    { id: 5, title: "Scared", image: "assets/images/cards/emotions/scared.png" },
-    { id: 6, title: "Excited", image: "assets/images/cards/emotions/excited.png" }
+    { id: 1, title: "Happy", image: "assets/images/emotions/happy.png" },
+    { id: 2, title: "Sad", image: "assets/images/emotions/sad.png" },
+    { id: 3, title: "Angry", image: "assets/images/emotions/angry.png" },
+    { id: 4, title: "Surprised", image: "assets/images/emotions/surprised.png" },
+    { id: 5, title: "Scared", image: "assets/images/emotions/scared.png" },
+    { id: 6, title: "Excited", image: "assets/images/emotions/excited.png" }
   ],
 
   weather: [
-    { id: 1, title: "Sunny", image: "assets/images/cards/weather/sunny.png" },
-    { id: 2, title: "Rainy", image: "assets/images/cards/weather/rainy.png" },
-    { id: 3, title: "Cloudy", image: "assets/images/cards/weather/cloudy.png" },
-    { id: 4, title: "Snowy", image: "assets/images/cards/weather/snowy.png" },
-    { id: 5, title: "Windy", image: "assets/images/cards/weather/windy.png" },
-    { id: 6, title: "Stormy", image: "assets/images/cards/weather/stormy.png" }
+    { id: 1, title: "Sunny", image: "assets/images/weather/sunny.png" },
+    { id: 2, title: "Rainy", image: "assets/images/weather/rainy.png" },
+    { id: 3, title: "Cloudy", image: "assets/images/weather/cloudy.png" },
+    { id: 4, title: "Snowy", image: "assets/images/weather/snowy.png" },
+    { id: 5, title: "Windy", image: "assets/images/weather/windy.png" },
+    { id: 6, title: "Stormy", image: "assets/images/weather/stormy.png" }
   ],
 
   family: [
-    { id: 1, title: "Mother", image: "assets/images/cards/family/mother.png" },
-    { id: 2, title: "Father", image: "assets/images/cards/family/father.png" },
-    { id: 3, title: "Brother", image: "assets/images/cards/family/brother.png" },
-    { id: 4, title: "Sister", image: "assets/images/cards/family/sister.png" },
-    { id: 5, title: "Grandmother", image: "assets/images/cards/family/grandmother.png" },
-    { id: 6, title: "Grandfather", image: "assets/images/cards/family/grandfather.png" }
+    { id: 1, title: "Mother", image: "assets/images/family/mother.png" },
+    { id: 2, title: "Father", image: "assets/images/family/father.png" },
+    { id: 3, title: "Brother", image: "assets/images/family/brother.png" },
+    { id: 4, title: "Sister", image: "assets/images/family/sister.png" },
+    { id: 5, title: "Grandmother", image: "assets/images/family/grandmother.png" },
+    { id: 6, title: "Grandfather", image: "assets/images/family/grandfather.png" }
 
   ]
 });
